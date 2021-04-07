c64830b1d2acb8b131d1d2d367da2bf158d09bc9
wang.haoyu <228233464@qq.com>
2021-04-07 22:26:54 +0800

    feat(router): router-view, tab-component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <m-header />
+  <m-tab />
+  <router-view />
 </template>
 <script>
 import MHeader from '@/components/header/header.vue'
+import MTab from '@/components/tab/index.vue'
 export default {
   components: {
-    MHeader
+    MHeader,
+    MTab
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Recommend from '@/views/recommend.vue'
+import Singer from '@/views/singer.vue'
+import TopList from '@/views/top-list.vue'
+import Search from '@/views/search.vue'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    redirect: '/recommend'
+  },
+  {
+    path: '/recommend',
+    name: 'Recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    name: 'Singer',
+    component: Singer
+  },
+  {
+    path: '/top-list',
+    name: 'TopList',
+    component: TopList
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
