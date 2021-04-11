d389d4fe5148a6e5f80f98d63585be6610df98e1
wang.haoyu <228233464@qq.com>
2021-04-11 22:38:54 +0800

    feat(loading): loading template & fix reset css for <a> style

---
+++ src/components/base/loading/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="loading">
+    <div class="loading-content">
+      <img width="24" height="24" src="./loading.gif" />
+      <p class="desc">{{ title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Loading',
+  data () {
+    return {
+      title: ''
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.loading {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0); // 百分比表示当前对象的尺寸
+  .loading-content {
+    text-align: center;
+    .desc {
+      line-height: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+    }
+  }
+}
+</style>
